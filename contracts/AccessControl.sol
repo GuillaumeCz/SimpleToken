d80efec61b1d3fd6911b84b80403d9be01a1c1bb
GuillaumeCz <guillaume.couzy@protonmail.com>
2019-09-03 00:14:59 +0200

    changes to npm scripts

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -1,34 +1,26 @@
 pragma solidity ^0.5.0;
 
 contract AccessControl {
-  mapping(address => bool) internal adminList;
+    mapping(address => bool) internal adminList;
 
-  modifier onlyAdmin() {
-    require(adminList[msg.sender]);
-    _;
-  }
+    modifier onlyAdmin() {
+        require(adminList[msg.sender]);
+        _;
+    }
 
-  constructor() public {
-    adminList[msg.sender] = true;
-  }
+    constructor() public {
+        adminList[msg.sender] = true;
+    }
 
-  function addAdmin(
-    address _addr
-  ) public onlyAdmin {
-    adminList[_addr] = true;
-  }
+    function addAdmin(address _addr) public onlyAdmin {
+        adminList[_addr] = true;
+    }
 
-  function removeAdmin(
-    address _addr
-  ) public onlyAdmin {
-    adminList[_addr] = false;
-  }
+    function removeAdmin(address _addr) public onlyAdmin {
+        adminList[_addr] = false;
+    }
 
-  function isAdmin(
-    address _addr
-  ) public view returns (
-    bool isAdmin_
-  ) {
-    isAdmin_ = adminList[_addr];
-  }
+    function isAdmin(address _addr) public view returns (bool isAdmin_) {
+        isAdmin_ = adminList[_addr];
+    }
 }
--- contracts/NameSystem.sol
+++ contracts/NameSystem.sol
@@ -3,66 +3,54 @@ pragma solidity ^0.5.0;
 import "./AccessControl.sol";
 
 contract NameSystem is AccessControl {
-  uint256 internal recordCount = 0;
+    uint256 internal recordCount = 0;
 
-  struct AddrNameRecord {
-    string name;
-    address addr;
-  }
-  
-  mapping(string => address) internal nameToAddr;
-  mapping(address => string) internal addrToName;
-  mapping(uint256 => AddrNameRecord) internal indexRecord;
+    struct AddrNameRecord {
+        string name;
+        address addr;
+    }
 
+    mapping(string => address) internal nameToAddr;
+    mapping(address => string) internal addrToName;
+    mapping(uint256 => AddrNameRecord) internal indexRecord;
 
-  function getName (
-    address _addr
-  ) public view returns (
-    string memory name_
-  ) {
-    require(_addr != address(0));
-    name_ = addrToName[_addr];
-  }
+    function getName(address _addr) public view returns (string memory name_) {
+        require(_addr != address(0));
+        name_ = addrToName[_addr];
+    }
 
+    function getAddr(string memory _name) public view returns (address addr_) {
+        require(bytes(_name).length != 0);
+        addr_ = nameToAddr[_name];
+    }
 
-  function getAddr (
-    string memory _name
-  ) public view returns (
-    address addr_
-  ) {
-    require(bytes(_name).length != 0);
-    addr_ = nameToAddr[_name];
-  }
+    function linkAddrToName(address _addr, string memory _name)
+        public
+        onlyAdmin
+        returns (bool success_)
+    {
+        require(
+            nameToAddr[_name] == address(0) &&
+                bytes(addrToName[_addr]).length == 0
+        );
+        addrToName[_addr] = _name;
+        nameToAddr[_name] = _addr;
+        indexRecord[recordCount++] = AddrNameRecord(_name, _addr);
+        success_ = true;
+    }
 
+    function getRecord(uint256 _id)
+        public
+        view
+        returns (string memory name_, address addr_)
+    {
+        AddrNameRecord memory record = indexRecord[_id];
+        name_ = record.name;
+        addr_ = record.addr;
+    }
 
-  function linkAddrToName (
-    address _addr,
-    string memory _name
-  ) public onlyAdmin returns (
-    bool success_
-  ){
-    require(nameToAddr[_name] == address(0) && bytes(addrToName[_addr]).length == 0);
-    addrToName[_addr] = _name;
-    nameToAddr[_name] = _addr;
-    indexRecord[recordCount++] = AddrNameRecord(_name, _addr);
-    success_ = true;
-  }
-
-
-  function getRecord(
-    uint256 _id
-  ) public view returns (
-    string memory name_,
-    address addr_
-  ) {
-    AddrNameRecord memory record = indexRecord[_id];
-    name_ = record.name;
-    addr_ = record.addr;
-  }
-
-
-  function getRecordCount() public view returns (uint256 counter_) {
-    counter_ = recordCount;
-  }
+    function getRecordCount() public view returns (uint256 counter_) {
+        counter_ = recordCount;
+    }
 
 }

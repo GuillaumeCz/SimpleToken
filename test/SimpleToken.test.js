447a632023f4c920631c556f2b73eb002222c6e3
GuillaumeCz <115c7a5fac@pm.me>
2020-01-07 14:56:15 +0100

    Rework tests code style

--- test/SimpleToken.test.js
+++ test/SimpleToken.test.js
@@ -13,60 +13,70 @@ contract("SimpleToken", accounts => {
       .then(() => instance.getCounter())
       .then(cpt => assert.equal(cpt, 1))});
 
-  it("...should fail when a non-admin user tries to create an SimpleToken", () =>
-    instance
+  it("...should fail when a non-admin user tries to create an SimpleToken", () => {
+    return instance
       .createSimpleToken(admin, user, details0, {from: user})
       .then(() => assert.isOk(false, "it should have failed... !"))
-      .catch(() => assert.isOk(true)));
+      .catch(() => assert.isOk(true))
+  });
 
-  it("...should fail when creating a SimpleToken with an unregistred user as destinator", () =>
-    instance
+  it("...should fail when creating a SimpleToken with an unregistred user as destinator", () => {
+    return instance
       .createSimpleToken(admin, unregistredUser, details0)
       .then(() => assert.isOk(false, "it should have failed... !"))
-      .catch(() => assert.isOk(true)));
+      .catch(() => assert.isOk(true));
+  });
 
-  it("...should fail when creating a SimpleToken with an unregistred user as the first owner", () =>
-    instance
+  it("...should fail when creating a SimpleToken with an unregistred user as the first owner", () => {
+    return instance
       .createSimpleToken(unregistredUser, admin, details0)
       .then(() => assert.isOk(false, "it should have failed... !"))
-      .catch(() => assert.isOk(true)));
+      .catch(() => assert.isOk(true))
+  });
 
-  it("...should get an owner's balance", () =>
-    instance.balanceOf(admin).then(balance => assert.equal(balance, 1)));
+  it("...should get an owner's balance", () => {
+    return instance.balanceOf(admin).then(balance => assert.equal(balance, 1))
+  });
 
-  it("...should check if admin is the onwer of the token 0", () =>
-    instance.ownerOf(0).then(owner => assert.equal(owner, admin)));
+  it("...should check if admin is the onwer of the token 0", () => {
+    return instance.ownerOf(0).then(owner => assert.equal(owner, admin))
+  });
 
-  it("...should get an SimpleToken with id 0", () =>
-    instance.getSimpleToken(0, {from: admin}).then(token => {
+  it("...should get an SimpleToken with id 0", () => {
+    return instance.getSimpleToken(0, {from: admin}).then(token => {
       assert.equal(admin, token.from);
       assert.equal(user, token.to);
       assert.equal(details0, token.details);
-    }));
+    })
+  });
 
-  it("...should fail if a non-admin or non-owner user tries to get a SimpleToken", () =>
-    instance
+  it("...should fail if a non-admin or non-owner user tries to get a SimpleToken", () => {
+    return instance
       .getSimpleToken(0, {from: user})
       .then(() => assert.isOk(false, "It should have failed... !"))
-      .catch(() => assert.isOk(true)));
+      .catch(() => assert.isOk(true))
+  });
 
-  it("..should create another SimpleToken by an admin, using 2 recorded users", () =>
-    instance
+  it("..should create another SimpleToken by an admin, using 2 recorded users", () => {
+    return instance
       .createSimpleToken(user, admin, details0)
       .then(() => instance.getCounter())
-      .then(cpt => assert.equal(cpt, 2)));
+      .then(cpt => assert.equal(cpt, 2))
+  });
 
-  it("...should check if user is the onwer of the token 1", () =>
-    instance.ownerOf(1).then(owner => assert.equal(owner, user)));
+  it("...should check if user is the onwer of the token 1", () => {
+    return instance.ownerOf(1).then(owner => assert.equal(owner, user))
+  });
 
-  it("...should get a SimpleToken if I am the owner", () =>
-    instance
+  it("...should get a SimpleToken if I am the owner", () => {
+    return instance
       .getSimpleToken(0, {from: user})
       .then(() => assert.isOk(true))
-      .catch(err => assert.isOk(false)));
+      .catch(err => assert.isOk(false))
+  });
 
-  it("...should transfer the SimpleToken 0 from admin to user", () =>
-    instance
+  it("...should transfer the SimpleToken 0 from admin to user", () => {
+    return instance
       .passToken(user, 0, {from: admin})
       .then(() => instance.ownerOf(0))
       .then(owner => {
@@ -79,14 +89,16 @@ contract("SimpleToken", accounts => {
       .then(([cptUser, cptAdmin]) => {
         assert.equal(cptAdmin, 0);
         assert.equal(cptUser, 2);
-      }));
+      });
+  });
 
-  it("...should burn the SimpleToken 0", () => 
-    instance.burnSimpleToken(0, { from: admin })
+  it("...should burn the SimpleToken 0", () =>  {
+    return instance.burnSimpleToken(0, { from: admin })
     .then(() => instance.getCounter())
     .then(cpt => {
       assert.equal(cpt, 1);
       return instance.balanceOf(user);
     })
-    .then(cpt => assert.equal(cpt, 1)));
+    .then(cpt => assert.equal(cpt, 1))
+  });
 });

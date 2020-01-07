f59fa4ac1e5d4c511fb170df8fba00335b08de57
GuillaumeCz <115c7a5fac@pm.me>
2020-01-07 10:16:58 +0100

    Add openzeppelin role lib

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -1,51 +1,55 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/access/Roles.sol";
+
 contract AccessControl {
-    mapping(address => bool) internal userList;
-    mapping(address => bool) internal adminList;
+    using Roles for Roles.Role;
+
+    Roles.Role private _users;
+    Roles.Role private _admins;
 
     modifier onlyAdmin() {
-        require(adminList[msg.sender]);
+        require(_admins.has(msg.sender), "DOES_NOT_HAVE_ADMIN_ROLE");
         _;
     }
 
     modifier existsAsUser(address _user) {
-        require(userList[_user]);
+        require(_users.has(_user), "DOES_NOT_HAVE_USER_ROLE");
         _;
     }
 
     modifier onlyUser() {
-        require(userList[msg.sender]);
+        require(_users.has(msg.sender), "DOES_NOT_HAVE_USER_ROLE");
         _;
     }
 
     constructor() public {
-        adminList[msg.sender] = true;
-        userList[msg.sender] = true;
+        _admins.add(msg.sender);
+        _users.add(msg.sender);
     }
 
     function addUser(address _addr) public onlyAdmin {
-        userList[_addr] = true;
+        _users.add(_addr);
     }
 
     function addAdmin(address _addr) public onlyAdmin {
-        adminList[_addr] = true;
+        _admins.add(_addr);
         addUser(_addr);
     }
 
     function removeUser(address _addr) public onlyAdmin {
-        userList[_addr] = false;
+        _users.remove(_addr);
     }
 
     function removeAdmin(address _addr) public onlyAdmin {
-        adminList[_addr] = false;
+        _admins.remove(_addr);
     }
 
-    function isUser(address _addr) public view returns (bool isUser_) {
-        isUser_ = userList[_addr];
+    function isUser(address _addr) public view returns (bool) {
+        return _users.has(_addr);
     }
 
-    function isAdmin(address _addr) public view returns (bool isAdmin_) {
-        isAdmin_ = adminList[_addr];
+    function isAdmin(address _addr) public view returns (bool) {
+        return _admins.has(_addr);
     }
 }

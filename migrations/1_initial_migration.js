f23b94dbc2761e71dd7b7b82e8c76d5d612cba1c
GuillaumeCz <115c7a5fac@pm.me>
2020-01-07 14:58:28 +0100

    Add migration file

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,8 +1,5 @@
 const Migrations = artifacts.require("Migrations");
 
-const SimpleToken = artifacts.require("SimpleToken");
-
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(SimpleToken, "SimpleToken", "SpTkn");
 };

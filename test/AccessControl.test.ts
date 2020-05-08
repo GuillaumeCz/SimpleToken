809ea67da149ac43462209f7597ab0c8cd1c5b54
GuillaumeCz <115c7a5fac@pm.me>
2020-05-08 11:02:36 +0200

    Adds types and fixes typos to AccessControl tests

--- test/AccessControl.test.ts
+++ test/AccessControl.test.ts
@@ -1,14 +1,15 @@
 import {SimpleTokenInstance} from "../types";
+import {Address} from "cluster";
 
 const Contract = artifacts.require("./SimpleToken");
 
 contract("AccessControl", accounts => {
-  const deployerAddr = accounts[0];
-  const admin0 = accounts[1];
-  const admin1 = accounts[2];
-  const user0 = accounts[3];
-  const user1 = accounts[4];
-  const user2 = accounts[5];
+  const deployerAddr: string = accounts[0];
+  const admin0: string = accounts[1];
+  const admin1: string = accounts[2];
+  const user0: string = accounts[3];
+  const user1: string = accounts[4];
+  const user2: string = accounts[5];
 
   let instance: SimpleTokenInstance;
 
@@ -19,7 +20,7 @@ contract("AccessControl", accounts => {
   );
 
   it("...should test if the user that deployed is an admin", () => {
-    return instance.isAdmin(deployerAddr).then(isAdmin => {
+    return instance.isAdmin(deployerAddr).then((isAdmin: boolean) => {
       assert.equal(isAdmin, true);
     });
   });
@@ -28,31 +29,31 @@ contract("AccessControl", accounts => {
     return instance
       .addAdmin(admin0)
       .then(() => instance.isAdmin(admin0))
-      .then(isAdmin => {
+      .then((isAdmin: boolean) => {
         assert.equal(isAdmin, true);
       });
   });
 
-  it("...should considere an admin as a user", () => {
-    return instance.isUser(admin0).then(isUser => {
+  it("...should consider an admin as a user", () => {
+    return instance.isUser(admin0).then((isUser: boolean) => {
       assert.equal(isUser, true);
     });
   });
 
-  it("...should autorize an admin to add a user", () => {
+  it("...should authorize an admin to add a user", () => {
     return instance
       .addUser(user0)
       .then(() => instance.isUser(user0))
-      .then(isUser => {
+      .then((isUser: boolean) => {
         assert.equal(isUser, true);
       });
   });
 
-  it("...should autorize an admin to add an admin", () => {
+  it("...should authorize an admin to add an admin", () => {
     return instance
       .addAdmin(admin1, {from: admin0})
       .then(() => instance.isAdmin(admin1))
-      .then(isAdmin => {
+      .then((isAdmin: boolean) => {
         assert.equal(isAdmin, true);
       });
   });
@@ -60,14 +61,19 @@ contract("AccessControl", accounts => {
   it("...should refuse that a user create another user", () => {
     return instance
       .addUser(user1, {from: user0})
-      .catch(err => assert.include(err.toString(), "Error"));
+      .then(() => {
+        assert.isOk(false)
+      })
+      .catch(() => {
+        assert.isOk(true)
+      });
   });
 
   it("...should authorize an admin to remove an user", () => {
     return instance
       .removeUser(user0, {from: admin0})
       .then(() => instance.isUser(user0))
-      .then(isUser => {
+      .then((isUser: boolean) => {
         assert.equal(isUser, false);
       });
   });
@@ -76,12 +82,12 @@ contract("AccessControl", accounts => {
     return instance
       .removeAdmin(admin1, {from: admin0})
       .then(() => instance.isAdmin(admin1))
-      .then(isAdmin => {
+      .then((isAdmin: boolean) => {
         assert.equal(isAdmin, false);
       });
   });
 
-  it("...should unauthorize a user to remove another user", () => {
+  it("...should un-authorize a user to remove another user", () => {
     return instance
       .addUser(user2)
       .then(() => instance.removeUser(user2, {from: user1}))
